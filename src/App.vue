0acd4a92b771fea9c949c58fcc8c444036d20cb5
wyc <897878045@qq.com>
2019-06-10 21:20:07 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    index
+
+    <el-button type="primary">主要按钮</el-button>
+
+    <p>
+      <router-view></router-view>
+    </p>
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+
+export default {
+  mounted(){
+
+    // 请求测试接口
+    // axios({
+    //   url: "https://api.github.com/users",
+    //   method: "GET",
+    //   // get请求参数
+    //   params: {
+    //     abc: 123
+    //   }
+    // }).then(res => {
+    //   console.log(res)
+    // })
+
+    // 请求测试接口
+    this.$axios({
+      url: "https://api.github.com/users",
+      method: "GET",
+    }).then(res => {
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
